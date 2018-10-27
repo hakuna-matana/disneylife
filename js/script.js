84415b3b14e8631218f5b542bfe1a9909cebcb7a
nastyarisa <pl_nastya@mail.ru>
2018-10-27 12:43:48 +0300

    slick carousel

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  $('.movie__list').slick({
+    slidesToShow: 7,
+    slidesToScroll: 2,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.movie__slider-arrow--left'),
+    nextArrow: $('.movie__slider-arrow--right'),
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 6,
+        }
+      },
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 5,
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
